b7ebf6be663e09c455eef48bab05e53ad3994566
Avadhutjc <Avadhutjc@gmail.com>
2021-07-11 23:08:26 +0530

    Facebook Login Page

---
+++ app/src/main/java/com/masai/a428june_facebook_login_page_css_you4/MainActivity.java
@@ -0,0 +1,14 @@
+package com.masai.a428june_facebook_login_page_css_you4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
